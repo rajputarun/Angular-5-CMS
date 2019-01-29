25bda1b1e12344f5e69b64cf45423919def742df
rajputarun <rajputarun0@gmail.com>
2019-01-29 13:58:00 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,6 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: "AIzaSyA3IZhoWj4ivRVhcQF4fCi7XWo8Ce7ydps",
-    authDomain: "cms-database-bd2e8.firebaseapp.com",
-    databaseURL: "https://cms-database-bd2e8.firebaseio.com",
-    projectId: "cms-database-bd2e8",
-    storageBucket: "cms-database-bd2e8.appspot.com",
-    messagingSenderId: "370097188968"
+   /*Firebase config goes here*/
   }
 };
